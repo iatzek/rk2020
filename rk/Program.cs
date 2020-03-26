77351238acb9c63ce23bdbd35b7ba7ae2fc36a1e
Jacek Kawa <jacek.kawa@polsl.pl>
2020-03-26 11:11:28 +0100

    poprawka na 2a dla gałęzi master

--- rk/Program.cs
+++ rk/Program.cs
@@ -9,8 +9,8 @@ namespace rk
             double a = 1, b = 0, c = -36;
             double delta = b * b - 4 * a * c;
 
-            double x1 = (-b - Math.Sqrt(delta)) / 2 * a;
-            double x2 = (-b - Math.Sqrt(delta)) / 2 * a;
+            double x1 = (-b - Math.Sqrt(delta)) / (2 * a);
+            double x2 = (-b - Math.Sqrt(delta)) / (2 * a);
 
             Console.WriteLine(String.Format("x1={0}, x2={1}", x1, x2));
 
