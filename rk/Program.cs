80fa37aca3c615da1ef8575ed12682beb9fce963
Jacek Kawa <jacek.kawa@polsl.pl>
2020-03-26 10:34:52 +0100

    pierwsza wersja z zakodowanymi parametrami

--- rk/Program.cs
+++ rk/Program.cs
@@ -6,7 +6,14 @@ namespace rk
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double a = 1, b = 0, c = -36;
+            double delta = b * b - 4 * a * c;
+
+            double x1 = (-b - Math.Sqrt(delta)) / 2 * a;
+            double x2 = (-b - Math.Sqrt(delta)) / 2 * a;
+
+            Console.WriteLine(String.Format("x1={0}, x2={1}", x1, x2));
+
         }
     }
 }
